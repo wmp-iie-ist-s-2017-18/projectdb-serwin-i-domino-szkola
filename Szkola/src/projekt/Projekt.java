18c8d9269a9ac7ad99656744bb05ced4d16232df
md5687 <47738234+md5687@users.noreply.github.com>
2019-05-03 23:20:14 +0200

    Logowanie

--- Szkola/src/projekt/Projekt.java
+++ Szkola/src/projekt/Projekt.java
@@ -15,7 +15,8 @@ public class Projekt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Logowannie logIn = new Logowannie();
+        logIn.setVisible(true);
     }
     
 }
