4bf93c46361b2488ea2c32befb530af4bbf7cbc1
monika <monika@lenovo>
2019-05-06 00:03:57 +0200

    Logowanie

--- Szkola/src/projekt/Logowanie.java
+++ Szkola/src/projekt/Logowanie.java
@@ -29,14 +29,22 @@ public class Logowanie extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        wybNauczyciel = new javax.swing.JCheckBox();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        wybNauczyciel = new javax.swing.JCheckBox();
-        wybUczen = new javax.swing.JCheckBox();
         ID = new javax.swing.JTextField();
         Zaloguj = new javax.swing.JButton();
         haslo = new javax.swing.JPasswordField();
+        UczenRB = new javax.swing.JRadioButton();
+        NauczycielRB = new javax.swing.JRadioButton();
+
+        wybNauczyciel.setText("Uczeń");
+        wybNauczyciel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                wybNauczycielActionPerformed(evt);
+            }
+        });
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -44,11 +52,26 @@ public class Logowanie extends javax.swing.JFrame {
 
         jLabel2.setText("Hasło: ");
 
-        wybNauczyciel.setText("Uczeń");
-
-        wybUczen.setText("Nauczyciel");
-
         Zaloguj.setText("Zaloguj");
+        Zaloguj.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ZalogujActionPerformed(evt);
+            }
+        });
+
+        UczenRB.setText("Uczeń");
+        UczenRB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                UczenRBActionPerformed(evt);
+            }
+        });
+
+        NauczycielRB.setText("Nauczyciel");
+        NauczycielRB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                NauczycielRBActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -57,21 +80,21 @@ public class Logowanie extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(51, 51, 51)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addGap(34, 34, 34)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(ID, javax.swing.GroupLayout.DEFAULT_SIZE, 166, Short.MAX_VALUE)
-                            .addComponent(haslo)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(wybNauczyciel)
-                        .addGap(29, 29, 29)
-                        .addComponent(wybUczen)
-                        .addGap(18, 18, 18)
-                        .addComponent(Zaloguj)))
-                .addContainerGap(36, Short.MAX_VALUE))
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addGap(34, 34, 34)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(ID, javax.swing.GroupLayout.DEFAULT_SIZE, 166, Short.MAX_VALUE)
+                    .addComponent(haslo))
+                .addContainerGap(46, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(UczenRB)
+                .addGap(18, 18, 18)
+                .addComponent(NauczycielRB)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(Zaloguj)
+                .addGap(30, 30, 30))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -84,12 +107,16 @@ public class Logowanie extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
                     .addComponent(haslo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(28, 28, 28)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(wybNauczyciel)
-                    .addComponent(wybUczen)
-                    .addComponent(Zaloguj))
-                .addContainerGap(53, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(UczenRB)
+                            .addComponent(NauczycielRB)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addComponent(Zaloguj)))
+                .addContainerGap(41, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -106,16 +133,37 @@ public class Logowanie extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void wybNauczycielActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_wybNauczycielActionPerformed
+        
+    }//GEN-LAST:event_wybNauczycielActionPerformed
+
+    private void UczenRBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UczenRBActionPerformed
+       if(evt.getSource().equals(UczenRB)){
+           NauczycielRB.setSelected(false);
+       }
+    }//GEN-LAST:event_UczenRBActionPerformed
+
+    private void ZalogujActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ZalogujActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ZalogujActionPerformed
+
+    private void NauczycielRBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NauczycielRBActionPerformed
+       if(evt.getSource().equals(NauczycielRB)){
+           UczenRB.setSelected(false);
+       }
+    }//GEN-LAST:event_NauczycielRBActionPerformed
+
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField ID;
+    private javax.swing.JRadioButton NauczycielRB;
+    private javax.swing.JRadioButton UczenRB;
     private javax.swing.JButton Zaloguj;
     private javax.swing.JPasswordField haslo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JCheckBox wybNauczyciel;
-    private javax.swing.JCheckBox wybUczen;
     // End of variables declaration//GEN-END:variables
 }
