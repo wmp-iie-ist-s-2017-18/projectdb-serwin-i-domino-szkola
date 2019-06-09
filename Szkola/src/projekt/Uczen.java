7732d933be2656560384c8422d015263f7ee0593
monika <monika@lenovo>
2019-06-10 01:07:18 +0200

    Aktualizacja pracy

--- Szkola/src/projekt/Uczen.java
+++ Szkola/src/projekt/Uczen.java
@@ -53,27 +53,40 @@ public class Uczen extends javax.swing.JFrame {
         Uczniowie u;
         u = uczniowieQ.SelectById(id);
         initComponents();
+        imieUczniaLabel.setText(u.getImie());
+        nazwiskoUcznia.setText(u.getNazwisko());
+        PESEL.setText(u.getPesel());
+        nr_legitymacji.setText(u.getNrLegitymacji());
+        dataurodzenia.setText((u.getDataUrodzenia()).toString());
+        miasto.setText(u.getMiasto());
+        ulica.setText(u.getUlica());
+        kodpocztowy.setText(u.getKodPocztowy());
+        tel_do_rodzica.setText(u.getNrTelefonuDoRodzica());
+        
         UwagiTableSelectAll();
         PrzedmiotyComboBox();
-        OcenyTableSelectAll();
+        PrzedmiotyTableAll();
+       
     }
 
     public void OcenyTableSelectAll() {
         queryOc = new OcenyQuery();
-        Object[] kolumna = {"Id oceny", " Data", "Ocena", "Opis"};
+        Object[] kolumna = {"Id oceny", "Id przedmiotu", " Data", "Ocena", "Opis"};
         DefaultTableModel model = new DefaultTableModel();
         model.setColumnIdentifiers(kolumna);
 
         try {
-
+            String str = (String)wybierzPrzedmiotOC.getSelectedItem();
+        int id_ucznia = Integer.parseInt(str);
             ocenki = queryOc.OcenyUcznia(id);
-            Object[] row = new Object[4];
+            Object[] row = new Object[5];
 
             for (Oceny o : ocenki) {
                 row[0] = o.getIdOceny();
-                row[1] = o.getDataWpisania().toString();
-                row[2] = o.getWartosc();
-                row[3] = o.getOpis();
+                row[1] = o.getPrzedmioty().getIdPrzedmioty();
+                row[2] = o.getDataWpisania().toString();
+                row[3] = o.getWartosc();
+                row[4] = o.getOpis();
 
                 model.addRow(row);
             }
@@ -83,6 +96,27 @@ public class Uczen extends javax.swing.JFrame {
 
         }
         TableOceny.setModel(model);
+    }
+public void PrzedmiotyTableAll() {
+        queryP = new PrzedmiotyQuery();
+        Object[] kolumny = {"Id przedmiotu", "Nazwa przedmiotu"};
+        DefaultTableModel modelP = new DefaultTableModel();
+        modelP.setColumnIdentifiers(kolumny);
+
+        try {
+            przedmiot = queryP.PrzedmiotySelectAll();
+            Object[] row = new Object[2];
+
+            for (Przedmioty p : przedmiot) {
+                row[0] = p.getIdPrzedmioty();
+                row[1] = p.getNazwaPrzedmiotu();
+
+                modelP.addRow(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Bład połączenia z bazą!");
+        }
+        PrzedmiotyTable.setModel(modelP);
     }
 
     public void PrzedmiotyComboBox() {
@@ -97,7 +131,7 @@ public class Uczen extends javax.swing.JFrame {
 
             for (Przedmioty p : przedmiot) {
 
-                wybierzPrzedmiotOC.addItem(p.getNazwaPrzedmiotu());
+                wybierzPrzedmiotOC.addItem(p.getIdPrzedmioty()+"");
 
             }
             session.close();
@@ -153,10 +187,33 @@ public class Uczen extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         TableOceny = new javax.swing.JTable();
         wylogujOc = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         TabelaUwagiUcz = new javax.swing.JTable();
         wylogujU = new javax.swing.JButton();
+        jPanel4 = new javax.swing.JPanel();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        PrzedmiotyTable = new javax.swing.JTable();
+        jPanel5 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        imieUczniaLabel = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        nazwiskoUcznia = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        PESEL = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        nr_legitymacji = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        dataurodzenia = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        miasto = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        ulica = new javax.swing.JLabel();
+        label9 = new javax.swing.JLabel();
+        kodpocztowy = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        tel_do_rodzica = new javax.swing.JLabel();
 
         jTable3.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -247,6 +304,13 @@ public class Uczen extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("Wyświetl");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -256,6 +320,8 @@ public class Uczen extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(57, 57, 57)
                 .addComponent(wybierzPrzedmiotOC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
@@ -269,13 +335,14 @@ public class Uczen extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(wylogujOc)
-                .addGap(2, 2, 2)
+                .addGap(1, 1, 1)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(wybierzPrzedmiotOC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(wybierzPrzedmiotOC, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 242, Short.MAX_VALUE)
-                .addGap(29, 29, 29))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(17, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Oceny", jPanel1);
@@ -328,11 +395,164 @@ public class Uczen extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addComponent(wylogujU)
                 .addGap(26, 26, 26)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(28, Short.MAX_VALUE))
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 202, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(25, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Uwagi", jPanel2);
+
+        PrzedmiotyTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
+            },
+            new String [] {
+                "Id przedmiotu", "Nazwa przedmiotu"
+            }
+        ));
+        jScrollPane4.setViewportView(PrzedmiotyTable);
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(64, Short.MAX_VALUE))
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(36, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Przedmioty", jPanel4);
+
+        jLabel2.setText("Imię:");
+
+        imieUczniaLabel.setText("jLabel3");
+
+        jLabel3.setText("Nazwisko:");
+
+        nazwiskoUcznia.setText("jLabel4");
+
+        jLabel4.setText("PESEL:");
+
+        PESEL.setText("jLabel5");
+
+        jLabel5.setText("Nr legitymacji:");
+
+        nr_legitymacji.setText("jLabel6");
+
+        jLabel6.setText("Data urodzenia:");
+
+        dataurodzenia.setText("jLabel7");
+
+        jLabel7.setText("Miasto:");
+
+        miasto.setText("jLabel8");
+
+        jLabel8.setText("Ulica:");
+
+        ulica.setText("jLabel9");
+
+        label9.setText("Kod pocztowy:");
+
+        kodpocztowy.setText("jLabel9");
+
+        jLabel9.setText("Nr telefonu do rodzica:");
+
+        tel_do_rodzica.setText("jLabel10");
+
+        javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
+        jPanel5.setLayout(jPanel5Layout);
+        jPanel5Layout.setHorizontalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel5Layout.createSequentialGroup()
+                .addGap(52, 52, 52)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel5Layout.createSequentialGroup()
+                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(dataurodzenia, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(74, 74, 74)
+                        .addComponent(tel_do_rodzica, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel5Layout.createSequentialGroup()
+                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(jPanel5Layout.createSequentialGroup()
+                                    .addComponent(jLabel4)
+                                    .addGap(18, 18, 18)
+                                    .addComponent(PESEL, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGroup(jPanel5Layout.createSequentialGroup()
+                                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addComponent(nazwiskoUcznia, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(jPanel5Layout.createSequentialGroup()
+                                    .addComponent(jLabel2)
+                                    .addGap(27, 27, 27)
+                                    .addComponent(imieUczniaLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                            .addGroup(jPanel5Layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(nr_legitymacji, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(32, 32, 32)
+                        .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(jPanel5Layout.createSequentialGroup()
+                                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jLabel7, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(miasto, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(ulica)))
+                            .addGroup(jPanel5Layout.createSequentialGroup()
+                                .addComponent(label9, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(kodpocztowy, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(86, Short.MAX_VALUE))
+        );
+        jPanel5Layout.setVerticalGroup(
+            jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel5Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(imieUczniaLabel)
+                    .addComponent(jLabel7)
+                    .addComponent(miasto))
+                .addGap(20, 20, 20)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(nazwiskoUcznia)
+                    .addComponent(jLabel8)
+                    .addComponent(ulica))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(PESEL)
+                    .addComponent(label9)
+                    .addComponent(kodpocztowy))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(nr_legitymacji)
+                    .addComponent(jLabel9))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(dataurodzenia)
+                    .addComponent(tel_do_rodzica))
+                .addContainerGap(112, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("Dane osobowe", jPanel5);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -344,9 +564,7 @@ public class Uczen extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 365, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -376,20 +594,47 @@ public class Uczen extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_wylogujOcActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        OcenyTableSelectAll();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel PESEL;
+    private javax.swing.JTable PrzedmiotyTable;
     private javax.swing.JTable TabelaUwagiUcz;
     private javax.swing.JTable TableOceny;
     private javax.swing.JButton WYLOGUJ_N;
+    private javax.swing.JLabel dataurodzenia;
+    private javax.swing.JLabel imieUczniaLabel;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTable jTable3;
+    private javax.swing.JLabel kodpocztowy;
+    private javax.swing.JLabel label9;
+    private javax.swing.JLabel miasto;
+    private javax.swing.JLabel nazwiskoUcznia;
+    private javax.swing.JLabel nr_legitymacji;
+    private javax.swing.JLabel tel_do_rodzica;
+    private javax.swing.JLabel ulica;
     private javax.swing.JComboBox<String> wybierzPrzedmiotOC;
     private javax.swing.JButton wylogujOc;
     private javax.swing.JButton wylogujU;
